d70a7cee16f3f546f82557feb586de4e0b89f681
Shubham Raj <shubhamraj2613@gmail.com>
2025-10-29 01:19:18 +0530

    Update useProduct.js

--- frontend/src/store/useProduct.js
+++ frontend/src/store/useProduct.js
@@ -2,8 +2,9 @@ import { create } from 'zustand';
 import axios from "axios";
 import toast from 'react-hot-toast';
 
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "";
+
 
 export const useProductStore = create((set, get) => ({
   products: [],
